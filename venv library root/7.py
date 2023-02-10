f467785777a98b692079b51a670b111a3d130d18
PERSONAL <danielgomezaristizabal2020@gmail.com>
2023-02-09 22:21:43 -0500

    Initial commit which includes a 1.py

---
+++ venv library root/7.py
@@ -0,0 +1,7 @@
+def MinMay(lista):
+  list= [1, 3, 55, 62, 44, 22, 78, 34, 75]
+  lista = list
+  print(f"El número mayor de la lista es: {max(list)}")
+  print(f"El número menor de la lista es: {min(list)}")
+
+MinMay(list)
