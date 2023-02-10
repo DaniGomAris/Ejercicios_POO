66c3192a91f9fac7e95ecf19b7f549d6ff534c0d
PERSONAL <danielgomezaristizabal2020@gmail.com>
2023-02-09 22:22:38 -0500

    Initial commit which includes a 1.py

---
+++ venv library root/13.py
@@ -0,0 +1,9 @@
+def operaciones():
+  Num1 = int(input('ingrese primer numero: '))
+  Num2 = int(input('ingrese segundo numero: '))
+  print('la suma es: ',Num1 + Num2)
+  print('la resta es: ',Num1 - Num2)
+  print('la division es: ',Num1 / Num2)
+  print('la multiplicacion es: ',Num1 * Num2)
+
+operaciones()
